f0a02dffd7fcb3efa1f74278d77de901ed90b821
Luciano Burgues <30976333+lucburg@users.noreply.github.com>
2023-03-28 20:55:53 +0200

    Update uploadfiles.py

--- uploadfiles.py
+++ uploadfiles.py
@@ -2,6 +2,6 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('Upload files')
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 query2 = "put file://my_file.txt @myinternal_stage auto_compress=true parallel=X"
-execute_query(conn, query2)
+execute_query(my_conn, query2)
