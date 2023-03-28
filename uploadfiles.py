59f935734b0bfa26539dc2756fc1d86adb4a08b6
Luciano Burgues <30976333+lucburg@users.noreply.github.com>
2023-03-28 22:46:00 +0200

    Update uploadfiles.py

--- uploadfiles.py
+++ uploadfiles.py
@@ -8,7 +8,7 @@ streamlit.title('Upload files')
 try:
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_cur = my_cnx.cursor()
-  my_cur.execute("put file://r"C:\Users\burguela\my_file.txt" @demo_db.public.my_internal_named_stage")
+  my_cur.execute("put file://C:\Users\burguela/my_file.txt @demo_db.public.my_internal_named_stage")
   my_cnx.close()
 except URLError as e:
   streamlit.error()
